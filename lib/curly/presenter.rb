e9d27ede678004d6a2b2acf0b10945f0cfda7e76
Daniel Schierbeck <dasch@zendesk.com>
2013-01-23 20:53:23 +0100

    Document Curly::Presenter

--- lib/curly/presenter.rb
+++ lib/curly/presenter.rb
@@ -36,6 +36,7 @@ module Curly
   #   presenter.author #=> "Jackie Chan"
   #
   class Presenter
+
     # Initializes the presenter with the given context and options.
     #
     # context - An ActionView::Base context.
@@ -48,18 +49,62 @@ module Curly
       end
     end
 
+    # The key that should be used to cache the view.
+    #
+    # Unless #cache_key returns nil, the result of rendering the template that
+    # the presenter supports will be cached. The return value will be part of
+    # the final cache key, along with a digest of the template itself.
+    #
+    # Any object can be used as a cache key, so long as it
+    #
+    # - is a String,
+    # - responds to #cache_key itself, or
+    # - is an Array of a Hash whose items themselves fit either of these
+    #   criteria.
+    #
+    # Returns the cache key Object or nil if no caching should be performed.
     def cache_key
       nil
     end
 
+    # The duration that the view should be cached for. Only relevant if
+    # #cache_key returns a non nil value.
+    #
+    # If nil, the view will not have an expiration time set.
+    #
+    # Examples
+    #
+    #   def cache_duration
+    #     10.minutes
+    #   end
+    #
+    # Returns the Fixnum duration of the cache item, in seconds, or nil if no
+    #   duration should be set.
     def cache_duration
       nil
     end
 
+    # Whether a method is available to templates rendered with the presenter.
+    #
+    # Templates can reference "variables", which are simply methods defined on
+    # the presenter. By default, only public instance methods can be
+    # referenced, and any method defined on Curly::Presenter itself cannot be
+    # referenced. This means that methods such as #cache_key and #inspect are
+    # not available. This is done for safety purposes.
+    #
+    # This policy can be changed by overriding this method in your presenters.
+    #
+    # method - The Symbol name of the method.
+    #
+    # Returns true if the method can be referenced by a template,
+    #   false otherwise.
     def method_available?(method)
       self.class.available_methods.include?(method)
     end
 
+    # A list of methods available to templates rendered with the presenter.
+    #
+    # Returns an Array of Symbol method names.
     def self.available_methods
       public_instance_methods - Curly::Presenter.public_instance_methods
     end
@@ -73,6 +118,9 @@ module Curly
       self.presented_names += args
     end
 
+    # Delegates private method calls to the current view context.
+    #
+    # The view context, an instance of ActionView::Base, is set by Rails.
     def method_missing(method, *args, &block)
       @_context.public_send(method, *args, &block)
     end
