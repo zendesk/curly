761cbdf4197c9d15ace96eb44c477d725e5c2b48
Daniel Schierbeck <dasch@zendesk.com>
2014-06-26 13:27:57 +0000

    Add a generator for creating presenter classes
    Usage: rails generate curly:presenter posts/show

--- lib/generators/curly/controller/controller_generator.rb
+++ lib/generators/curly/controller/controller_generator.rb
@@ -4,7 +4,7 @@ require 'rails/generators/named_base'
 module Curly
   module Generators
     class ControllerGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
 
       argument :actions, type: :array, default: [], banner: "action action"
 
