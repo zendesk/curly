f80626c9aded3855b6997fd681e8f718b6a0283f
Daniel Schierbeck <dasch@zendesk.com>
2014-06-29 15:01:58 +0000

    Add an integration spec for conditional blocks

---
+++ spec/integration/conditional_blocks_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Conditional block components" do
+  include RenderingSupport
+
+  example "with neither identifier nor attributes" do
+    presenter do
+      def high?
+        true
+      end
+
+      def low?
+        false
+      end
+    end
+
+    render("{{#high?}}yup{{/high?}}").should == "yup"
+    render("{{#low?}}nah{{/low?}}").should == ""
+  end
+
+  example "with an identifier" do
+    presenter do
+      def even?(number)
+        number.to_i % 2 == 0
+      end
+    end
+
+    render("{{#even.42?}}even{{/even.42?}}").should == "even"
+    render("{{#even.13?}}even{{/even.13?}}").should == ""
+  end
+
+  example "with attributes" do
+    presenter do
+      def square?(width:, height:)
+        width.to_i == height.to_i
+      end
+    end
+
+    render("{{#square? width=2 height=2}}square{{/square?}}").should == "square"
+    render("{{#square? width=3 height=2}}square{{/square?}}").should == ""
+  end
+end
