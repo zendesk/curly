b0b469dc383343163332ea2914e09799da0b49eb
Jeremy Rodi <redjazz96@gmail.com>
2014-07-27 20:07:27 -0400

    Clean up line numbers
    Only determine the line an error occurred on when the
    error occurs.

--- lib/curly/syntax_error.rb
+++ lib/curly/syntax_error.rb
@@ -2,15 +2,16 @@ require 'curly/error'
 
 module Curly
   class SyntaxError < Error
-    def initialize(position, source, line)
-      @position, @source, @line = position, source, line
+    def initialize(position, source)
+      @position, @source = position, source
     end
 
     def message
-      start = [@position - 8, 0].max
-      stop = [@position + 8, @source.length].min
+      start   = [@position - 8, 0].max
+      stop    = [@position + 8, @source.length].min
       snippet = @source[start..stop].strip
-      "invalid syntax near `#{snippet}` on line #{@line} in " \
+      line    = @source[0..@position].count("\n") + 1
+      "invalid syntax near `#{snippet}` on line #{line} in " \
         "template:\n\n#{@source}\n"
     end
   end
--- spec/syntax_error_spec.rb
+++ spec/syntax_error_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Curly::SyntaxError, "#message" do
   it "includes the context of the error in the message" do
     source = "I am a very bad error that has snuck in"
-    error = Curly::SyntaxError.new(13, source, 3)
+    error = Curly::SyntaxError.new(13, source)
 
     error.message.should == <<-MESSAGE.strip_heredoc
-      invalid syntax near `a very bad error` on line 3 in template:
+      invalid syntax near `a very bad error` on line 1 in template:
 
       I am a very bad error that has snuck in
     MESSAGE
